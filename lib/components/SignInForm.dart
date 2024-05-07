20a237e846e9e43d8388d28b8b53b9d2fdf8b36e
saintkim12 <saintkim1232@gmail.com>
2024-05-07 18:00:51 +0900

    작업사항저장

---
+++ lib/components/SignInForm.dart
@@ -0,0 +1,80 @@
+// import 'dart:io';
+import 'dart:js_util';
+
+import 'package:flutter/material.dart';
+
+///
+/// @NOTE
+/// 상태가 포함된 form + state
+/// ref: https://docs.flutter.dev/cookbook/forms/validation
+///
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  SignInFormState createState() {
+    return SignInFormState();
+  }
+}
+
+class SignInFormState extends State<SignInForm> {
+  // form의 고유한 키
+  final _formKey = GlobalKey<FormState>();
+  final fieldId = TextEditingController();
+  final fieldPassword = TextEditingController();
+
+  // final httpClient = HttpClient();
+
+  Future<void> onSignIn() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text('Sign in...')));
+    // Future fetching = Future.delayed(const Duration(seconds: 3));
+    return Future(
+      () {},
+    ).timeout(const Duration(seconds: 3)).then((_) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Sign In: ${fieldId.text}')));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+              controller: fieldId,
+              decoration: const InputDecoration(labelText: "ID"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              }),
+          TextFormField(
+              controller: fieldPassword,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              }),
+          Padding(
+              padding: const EdgeInsets.symmetric(
+                  // padding top+bottom or left+right
+                  vertical: 16),
+              child: ElevatedButton(
+                  onPressed: onSignIn, child: const Text("Submit")))
+        ],
+      ),
+    );
+  }
+}
